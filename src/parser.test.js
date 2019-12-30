1b479e5ce60adfd16da2a512341b6ed84aed5aec
tmt <thomas.culdaut@gmail.com>
2019-12-30 07:40:48 +0100

    md correct indent

--- src/parser.test.js
+++ src/parser.test.js
@@ -3,15 +3,16 @@ import parser from './parser'
 describe('parser', () => {
   it('parse markdown', () => {
     const md = `# Choose your story
-    
-    This is a test adventure, welcome.
-    
-    ## The space ship
-    
-    You are the brave pilot of this ship.
-    The trip is long and boring.
-    
-    Are you going to [pee](/space-toilet) or to [get a rest](/bedroom)`
+
+This is a test adventure, welcome.
+
+## The space ship
+
+You are the brave pilot of this ship.
+The trip is long and boring.
+
+Are you going to [pee](/space-toilet) or to [get a rest](/bedroom)`
+
     expect(parser(md)).toMatchSnapshot()
   })
 })
