0c17766b4bf1650b282cc06437c8f18dac1ef3e7
tmt <thomas.culdaut@gmail.com>
2019-12-31 00:30:26 +0100

    need refactor scene naming

--- src/player.js
+++ src/player.js
@@ -1,13 +1,24 @@
 import * as R from 'ramda'
 
+export const getTarget = R.pipe(R.prop('target'), R.tail)
+
+export const matchTarget = move => R.propEq('name', getTarget(move))
+
+// To do : write a getSceneName
+export const findScene = move => R.filter(matchTarget(move))
+
 const player = (book, moves = []) => {
   const [firstScene, ...scenes] = book
   return [
     firstScene,
-    moves.reduce((acc, { type }) => {
-      switch (type) {
+    ...moves.reduce((acc, move) => {
+      switch (move.type) {
         case 'start':
-          return R.head(scenes)
+          return [R.head(scenes)]
+        case 'anchor':
+          return [...acc, findScene(move)(book)]
+        default:
+          return acc
       }
     }, []),
   ]
--- src/player.test.js
+++ src/player.test.js
@@ -1,14 +1,50 @@
 import parser from './parser'
-import player from './player'
+import player, { findScene, getTarget, matchTarget } from './player'
 import adventure from './adventure.md.js'
 import book from './book'
 
+const adventureBook = book(parser(adventure))
+const startMove = {
+  type: 'start',
+}
+const moveToCantina = {
+  type: 'anchor',
+  target: '/cantina',
+}
+
 describe('player', () => {
   it('play book introduction if no actions', () => {
-    expect(player(book(parser(adventure)))).toMatchSnapshot()
+    expect(player(adventureBook)).toMatchSnapshot()
   })
   it('play second level 2 scene on start', () => {
-    const moves = [{ type: 'start' }]
-    expect(player(book(parser(adventure)), moves)).toMatchSnapshot()
+    const moves = [startMove]
+    expect(player(adventureBook, moves)).toMatchSnapshot()
+  })
+  it('play cantina scene on cantina target', () => {
+    const moves = [startMove, moveToCantina]
+    expect(player(adventureBook, moves)).toMatchSnapshot()
+  })
+})
+
+describe('findScene', () => {
+  it('find a scene', () => {
+    expect(findScene(moveToCantina)(adventureBook)).toMatchSnapshot()
+  })
+})
+
+describe('matchTarget', () => {
+  it('match cantina scene', () => {
+    const cantinaScene = { content: adventureBook[2], name: 'cantina' }
+    expect(matchTarget(moveToCantina)(cantinaScene)).toBe(true)
+  })
+  it('does not match cantina scene', () => {
+    const cantinaScene = { content: adventureBook[1], name: 'the-space-ship' }
+    expect(matchTarget(moveToCantina)(cantinaScene)).toBe(false)
+  })
+})
+
+describe('getTarget', () => {
+  it('get move target', () => {
+    expect(getTarget(moveToCantina)).toBe('cantina')
   })
 })
