4c4f4636cdf00dc6398c13d8b2d1fa1098c8fad4
tmt <thomas.culdaut@gmail.com>
2019-12-30 07:55:00 +0100

    move test adventure to md.js

---
+++ src/adventure.md.js
@@ -0,0 +1,10 @@
+export default `# Choose your story
+
+This is a test adventure, welcome.
+
+## The space ship
+
+You are the brave pilot of this ship.
+The trip is long and boring.
+
+Are you going to [pee](/space-toilet) or to [get a rest](/bedroom)`
