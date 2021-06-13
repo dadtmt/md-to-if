10675ec26cadcaf6a89f723aae25d2e9a2e52c9a
dadtmt <thomas.culdaut@gmail.com>
2021-06-14 00:12:58 +0200

    render the adventure title

--- src/react-renderer.test.ts
+++ src/react-renderer.test.ts
@@ -1,3 +1,9 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import '@testing-library/jest-dom'
+
 import { render, screen } from '@testing-library/react'
 
 import book from './book'
@@ -7,18 +13,13 @@ import renderer from './react-renderer'
 import player from './player'
 
 const adventureBook = book(parser(adventure))
-const startMove = {
-  type: 'start',
-}
 
-describe('React render', ()=>{
+describe('React render', () => {
+  render(renderer(player(adventureBook)))
 
-    const output = render(
-        renderer(
-            player(adventureBook)
-    ))
-
-    it('render the adventure title', () => {
-        expect(output.getByRole('heading', { level: 1 }))
-    })
+  it('render the adventure title', () => {
+    expect(screen.queryByRole('heading', { level: 1 })).toHaveTextContent(
+      'Choose your story'
+    )
+  })
 })
