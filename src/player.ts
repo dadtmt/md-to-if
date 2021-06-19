ddc2254d54faa72c8643336937421a28da65bdff
dadtmt <thomas.culdaut@gmail.com>
2021-06-19 22:14:40 +0200

    add empty state to intro

--- src/player.ts
+++ src/player.ts
@@ -5,15 +5,19 @@ export interface Move {
   type: string
   [prop: string]: any
 }
+
 export type PlayedScene = Scene & { state: State }
 
 // [Scene], [Move] -> [PlayedScene]
-const player: (scenes: Scene[], moves?: Move[]) => Scene[] = (
+const player: (scenes: Scene[], moves?: Move[]) => PlayedScene[] = (
   scenes,
   moves
 ) => {
   const [introduction, ...scenesAfterInroduction] = scenes
-  return [introduction, ...playMoves(moves ?? [], scenesAfterInroduction)]
+  return [
+    { ...introduction, state: {} },
+    ...playMoves(moves ?? [], scenesAfterInroduction)
+  ]
 }
 
 export default player
