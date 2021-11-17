0594d97040223698cadd89914a7c9740f9a41d1a
dadtmt <thomas.culdaut@gmail.com>
2021-11-17 18:37:11 +0100

    adds the edited action scene

--- src/editor/editScene.test.tsx
+++ src/editor/editScene.test.tsx
@@ -69,4 +69,111 @@ describe('editScene', () => {
       expect(editScene(book.children, editedScene)).toEqual(expected.children)
     })
   })
+  describe('when edit an actionscene', () => {
+    const book = (
+      <book>
+        <scene name="story_title">
+          <hh1>Story title</hh1>
+          <hp>Description</hp>
+        </scene>
+        <scene
+          name="first_scene"
+          path="/first_scene"
+          dialog={{
+            actions: [
+              <scene name="action1" path="/first_scene/action1">
+                <hh3>Action 1</hh3>
+                <hp>Action 1 text</hp>
+              </scene>,
+              <scene name="action2" path="/first_scene/action2">
+                <hh3>Action 2</hh3>
+                <hp>Action 2 text</hp>
+              </scene>
+            ],
+            quote: <hblockquote>first scene dialog</hblockquote>
+          }}>
+          <hh2>First scene</hh2>
+          <hp>first scene text</hp>
+        </scene>
+      </book>
+    ) as any as { children: EditScene[] }
+
+    it('replaces the edited action scene', () => {
+      const editedScene = (
+        <scene name="action1" path="/first_scene/action1">
+          <hh3>Action 1</hh3>
+          <hp>Action 1 edited text</hp>
+        </scene>
+      ) as any as EditScene
+      const expected = (
+        <book>
+          <scene name="story_title">
+            <hh1>Story title</hh1>
+            <hp>Description</hp>
+          </scene>
+          <scene
+            name="first_scene"
+            path="/first_scene"
+            dialog={{
+              actions: [
+                <scene name="action1" path="/first_scene/action1">
+                  <hh3>Action 1</hh3>
+                  <hp>Action 1 edited text</hp>
+                </scene>,
+                <scene name="action2" path="/first_scene/action2">
+                  <hh3>Action 2</hh3>
+                  <hp>Action 2 text</hp>
+                </scene>
+              ],
+              quote: <hblockquote>first scene dialog</hblockquote>
+            }}>
+            <hh2>First scene</hh2>
+            <hp>first scene text</hp>
+          </scene>
+        </book>
+      ) as any as { children: EditScene[] }
+      expect(editScene(book.children, editedScene)).toEqual(expected.children)
+    })
+
+    it('adds the edited action scene', () => {
+      const editedScene = (
+        <scene name="action3" path="/first_scene/action3">
+          <hh3>Action 3</hh3>
+          <hp>Action 3</hp>
+        </scene>
+      ) as any as EditScene
+      const expected = (
+        <book>
+          <scene name="story_title">
+            <hh1>Story title</hh1>
+            <hp>Description</hp>
+          </scene>
+          <scene
+            name="first_scene"
+            path="/first_scene"
+            dialog={{
+              actions: [
+                <scene name="action1" path="/first_scene/action1">
+                  <hh3>Action 1</hh3>
+                  <hp>Action 1 text</hp>
+                </scene>,
+                <scene name="action2" path="/first_scene/action2">
+                  <hh3>Action 2</hh3>
+                  <hp>Action 2 text</hp>
+                </scene>,
+                <scene name="action3" path="/first_scene/action3">
+                  <hh3>Action 3</hh3>
+                  <hp>Action 3</hp>
+                </scene>
+              ],
+              quote: <hblockquote>first scene dialog</hblockquote>
+            }}>
+            <hh2>First scene</hh2>
+            <hp>first scene text</hp>
+          </scene>
+        </book>
+      ) as any as { children: EditScene[] }
+      expect(editScene(book.children, editedScene)).toEqual(expected.children)
+    })
+  })
 })
--- src/editor/editScene.ts
+++ src/editor/editScene.ts
@@ -2,20 +2,46 @@ export interface EditScene {
   type: 'scene'
   name: string
   path: string
+  dialog?: {
+    actions: EditScene[]
+  }
 }
 
 const editScene = (
   scenes: EditScene[],
-  editedScene: EditScene
+  editedScene: EditScene,
+  lookedPath?: string[]
 ): EditScene[] => {
   const { path: editedScenePath } = editedScene
-  const existingScene = scenes.find(({ path }) => path === editedScenePath)
+  const [nameToLook, ...restOfPath] =
+    lookedPath ?? editedScenePath.split('/').splice(1)
+  const existingScene = scenes.find(({ name }) => name === nameToLook)
+  if (restOfPath.length === 0) {
+    return existingScene !== undefined
+      ? scenes.map((scene) => {
+          const { path } = scene
+          return path === editedScenePath ? editedScene : scene
+        })
+      : [...scenes, editedScene]
+  }
   return existingScene !== undefined
     ? scenes.map((scene) => {
-        const { path } = scene
-        return path === editedScenePath ? editedScene : scene
+        const { dialog, name } = scene
+        if (dialog === undefined) {
+          return scene
+        }
+        const { actions } = dialog
+        return nameToLook === name
+          ? {
+              ...scene,
+              dialog: {
+                ...dialog,
+                actions: editScene(actions, editedScene, restOfPath)
+              }
+            }
+          : scene
       })
-    : [...scenes, editedScene]
+    : scenes
 }
 
 export default editScene
