af312fc05b358154c3da9e8f6c4845b593fc3e07
dadtmt <thomas.culdaut@gmail.com>
2021-11-18 20:29:16 +0100

    refactor editScene

--- src/editor/editScene.ts
+++ src/editor/editScene.ts
@@ -16,32 +16,28 @@ const editScene = (
   const [nameToLook, ...restOfPath] =
     lookedPath ?? editedScenePath.split('/').splice(1)
   const existingScene = scenes.find(({ name }) => name === nameToLook)
-  if (restOfPath.length === 0) {
-    return existingScene !== undefined
-      ? scenes.map((scene) => {
-          const { path } = scene
-          return path === editedScenePath ? editedScene : scene
-        })
-      : [...scenes, editedScene]
-  }
-  return existingScene !== undefined
-    ? scenes.map((scene) => {
-        const { dialog, name } = scene
-        if (dialog === undefined) {
-          return scene
-        }
-        const { actions } = dialog
-        return nameToLook === name
-          ? {
-              ...scene,
-              dialog: {
-                ...dialog,
-                actions: editScene(actions, editedScene, restOfPath)
-              }
+  if (existingScene !== undefined) {
+    return scenes.map((scene) => {
+      const { dialog, name, path } = scene
+      if (restOfPath.length === 0) {
+        return path === editedScenePath ? editedScene : scene
+      }
+      if (dialog === undefined) {
+        return scene
+      }
+      const { actions } = dialog
+      return nameToLook === name
+        ? {
+            ...scene,
+            dialog: {
+              ...dialog,
+              actions: editScene(actions, editedScene, restOfPath)
             }
-          : scene
-      })
-    : scenes
+          }
+        : scene
+    })
+  }
+  return restOfPath.length === 0 ? [...scenes, editedScene] : scenes
 }
 
 export default editScene
